7a030e488e094f8a9169dcad15c3fd384237b6cb
Jim Veneskey <github@strifecrafter.com>
2015-04-20 12:48:23 -0400

    Added a third view accessible from the tab bar - but it needs customized still

---
+++ Tabbed Example/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tabbed Example
+//
+//  Created by jim Veneskey on 4/20/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
